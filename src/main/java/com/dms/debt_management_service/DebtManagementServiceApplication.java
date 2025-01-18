f2ad5d17ae30066137bf5360b69397ef53a1c766
dhidalgomo <dumar.hidalgo@amaris.com>
2025-01-18 08:53:56 -0500

    Initial commit

---
+++ src/main/java/com/dms/debt_management_service/DebtManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dms.debt_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebtManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebtManagementServiceApplication.class, args);
+	}
+
+}
