22b81378338e58d04836067bbfa01d150b3d2766
dhidalgomo <dumar.hidalgo@amaris.com>
2025-01-18 09:28:44 -0500

    Initialize spring web and main debt controller as well as yml files

---
+++ src/main/java/com/dms/debt_management_service/controller/DebtController.java
@@ -0,0 +1,9 @@
+package com.dms.debt_management_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("${controller.properties.base-path-v1}/debt")
+public class DebtController {
+}
